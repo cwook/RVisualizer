07068e958054d10dffe36c364f484323919d560a
dereklung1996 <dereklung1996@gmail.com>
2014-11-05 15:40:34 -0800

    Made the visuals more random
    - created a folder for songs
    - used random to create visuals

--- visualizer.rkt
+++ visualizer.rkt
@@ -7,9 +7,10 @@
 (require 2htdp/universe)
 (require racket/base)
 
-(define SONG-LOCATION "C:/tmp/rct2theme.wav")
+;(define SONG-LOCATION "C:/tmp/rct2theme.wav")
+(define SONG-LOCATION "songs/The Intro.wav")
 
-(define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 10)))
+(define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 120)))
 
 (define ps (make-pstream))
 
@@ -20,12 +21,26 @@
   )
 
 (define (draw w)
-  (overlay
-   (triangle (* 200 w) "solid" "black")
-   (empty-scene 200 200))
-  )
+  (place-image
+   (circle (* 30 w) "solid" (make-color (random 255) (random 255) (random 255)))
+   (random 600) 100
+   (place-image
+    (circle (* 120 w) "solid" (make-color (random 255) (random 255) (random 255)))
+    (random 600) 50
+    (place-image
+    (circle (* 45 w) "solid" (make-color (random 255) (random 255) (random 255)))
+    (random 600) 100
+     (place-image
+    (circle (* 180 w) "solid" (make-color (random 255) (random 255) (random 255)))
+    (random 600) 100
+    (place-image
+    (circle (* 20 w) "solid" (make-color (random 255) (random 255) (random 255)))
+    (random 600) 50
+   (empty-scene 600 200)))))))
+  
 
 (pstream-queue ps SONG 0)
 (big-bang 0 [on-tick tock] [to-draw draw])
+
 
 
