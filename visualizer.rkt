6ec9152b9e8c71ed750d5986997661377b4d045f
Christian Wookey <christian.wookey@gmail.com>
2014-11-11 14:19:52 -0800

    Mouse event done

--- visualizer.rkt
+++ visualizer.rkt
@@ -1,56 +1,100 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname visualizer) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "arrow.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "arrow.rkt" "teachpack" "htdp")))))
-
 (require rsound)
 (require rsound/draw)
 (require 2htdp/universe)
 (require racket/base)
 
-(define SONG-LOCATION "C:/tmp/rct2theme.wav")
-;(define SONG-LOCATION "songs/The Intro.wav")
+;(define SONG-LOCATION "C:/tmp/rct2theme.wav")
+(define SONG-LOCATION "songs/The Intro.wav")
 
-(define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 10)))
+(define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 120)))
 
-(define-struct world[t a c1now c1go])
+(define-struct world[t a c1now c1go slide-h slide-v drag?])
 
-(define INITIAL-WORLD (make-world 0 0 300 300))
+
+
+(define INITIAL-WORLD (make-world 0 0 300 300 900 650 false))
 
 (define ps (make-pstream))
 
 (define (both a b) b)
 
 (define (tock w)
-  (cond
-    [(= 0 (remainder (world-t w) 5)) 
-     (begin
-       (print "0")
-     (make-world
-      (add1 (world-t w))
-      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
-      (/ (+ (world-c1now w) (world-c1go w)) 2)
-      (* (world-a w) 300)))
-     ]
-    [else 
-     (begin
-       (print "1")
-     (make-world
-      (add1 (world-t w))
-      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
-      (/ (+ (world-c1now w) (world-c1go w)) 2)
-      (world-c1go w)))
-     ]
-  ))
+ (cond
+   [(= 0 (remainder (world-t w) 5))
+    (make-world
+     (add1 (world-t w))
+     (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
+     (/ (+ (world-c1now w) (world-c1go w)) 2)
+     (* (world-a w) 100)(world-slide-h w)(world-slide-v w)(world-drag? w))
+    ]
+   [else
+    (make-world
+     (add1 (world-t w))
+     (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
+     (/ (+ (world-c1now w) (world-c1go w)) 2)
+     (world-c1go w)(world-slide-h w)(world-slide-v w)(world-drag? w))
+    ]
+ ))
 
 (define (draw w)
   (place-image
-   (circle (world-c1now w) "solid" (make-color 0 0 0))
-   600 200
-   (empty-scene 1200 400)))
+  (circle (world-c1now w) "solid" (make-color 0 0 0))
+  600 320
+  (place-image
+  (square 20 "solid" "green")
+  (world-slide-h w) (world-slide-v w)
+  (place-image
+  (rectangle 1100 500 "outline" "black")
+  600 320
+  (place-image
+  (square 50 "solid" "green")
+  100 650
+  (place-image
+  (square 50 "solid" "red")
+  200 650
+  (place-image
+  (rectangle 500 20 "outline" "black")
+  900 650
+  (empty-scene 1200 720))))))))
+
+(define (mouse-event w x y event)
+ (begin
+   (print (number->string x))
+   
+ (cond
+   [(mouse=? event "button-up")
+    (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) false)
+    ]
+   [(mouse=? event "drag")
+    (cond
+      [(boolean=? true (world-drag? w))
+       (cond
+         [(and (> x (- 900 250)) (< x (+ 900 250)))
+          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) x (world-slide-v w) true)]
+         [else 
+          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) true)]
+         )]
+      [else 
+       (cond
+         [(and
+           (and (> x (- (world-slide-h w) 10)) (< x (+ (world-slide-h w) 10)))
+           (and (> y (- (world-slide-v w) 10)) (< y (+ (world-slide-v w) 10))))
+          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w)
+                      x (world-slide-v w) true)]
+         [else w])])
+    ]
+   [else w])
+ ))
+
+
 
 
 (pstream-queue ps SONG 0)
-(big-bang INITIAL-WORLD [on-tick tock] [to-draw draw] [state true])
-
-
-
+(big-bang INITIAL-WORLD
+         [on-tick tock]
+         [to-draw draw]
+         [on-mouse mouse-event]
+         [state true])
