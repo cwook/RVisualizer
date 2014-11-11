49cdcdf2282a92b8e696f492c7b073b22a7262d9
Christian Wookey <christian.wookey@gmail.com>
2014-11-11 15:08:14 -0800

    circle color changes with slider value

--- visualizer.rkt
+++ visualizer.rkt
@@ -11,37 +11,40 @@
 
 (define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 120)))
 
-(define-struct world[t a c1now c1go slide-h slide-v drag?])
+(define-struct world[t a c1now c1go slide-h slide-v drag? c1r c1g c1b])
 
 
 
-(define INITIAL-WORLD (make-world 0 0 300 300 900 650 false))
+(define INITIAL-WORLD (make-world 0 0 300 300 900 650 false 100 100 100))
 
 (define ps (make-pstream))
 
 (define (both a b) b)
 
 (define (tock w)
+  (begin
+    (print (number->string (/ (- (world-slide-h w) 652) 494)))
  (cond
    [(= 0 (remainder (world-t w) 5))
     (make-world
      (add1 (world-t w))
      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
      (/ (+ (world-c1now w) (world-c1go w)) 2)
-     (* (world-a w) 100)(world-slide-h w)(world-slide-v w)(world-drag? w))
+     (* (world-a w) 400)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
     ]
    [else
     (make-world
      (add1 (world-t w))
      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
      (/ (+ (world-c1now w) (world-c1go w)) 2)
-     (world-c1go w)(world-slide-h w)(world-slide-v w)(world-drag? w))
+     (world-c1go w)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
     ]
- ))
+ )))
+
 
 (define (draw w)
   (place-image
-  (circle (world-c1now w) "solid" (make-color 0 0 0))
+  (circle (world-c1now w) "solid" (make-color (world-c1r w)(world-c1g w)(world-c1b w)))
   600 320
   (place-image
   (square 20 "solid" "green")
@@ -60,34 +63,30 @@
   900 650
   (empty-scene 1200 720))))))))
 
+
 (define (mouse-event w x y event)
- (begin
-   (print (number->string x))
-   
  (cond
    [(mouse=? event "button-up")
-    (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) false)
+    (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) false (world-c1r w)(world-c1g w)(world-c1b w))
     ]
    [(mouse=? event "drag")
     (cond
       [(boolean=? true (world-drag? w))
        (cond
-         [(and (> x (- 900 250)) (< x (+ 900 250)))
-          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) x (world-slide-v w) true)]
+         [(and (> x (- 910 250)) (< x (+ 890 250)))
+          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) x (world-slide-v w) true (round (* 255 (/ (- (world-slide-h w) 660) 480))) (world-c1g w)(world-c1b w))]
          [else 
-          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) true)]
+          (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w) (world-slide-h w) (world-slide-v w) true (world-c1r w)(world-c1g w)(world-c1b w))]
          )]
       [else 
        (cond
-         [(and
-           (and (> x (- (world-slide-h w) 10)) (< x (+ (world-slide-h w) 10)))
-           (and (> y (- (world-slide-v w) 10)) (< y (+ (world-slide-v w) 10))))
+         [(and (> x (- 910 250)) (< x (+ 890 250)))
           (make-world (world-t w) (world-a w) (world-c1now w) (world-c1go w)
-                      x (world-slide-v w) true)]
+                      x (world-slide-v w) true (world-c1r w)(world-c1g w)(world-c1b w))]
          [else w])])
     ]
    [else w])
- ))
+ )
 
 
 
