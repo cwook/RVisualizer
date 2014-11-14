2ebff537e6b47e59543ff384b0b58ab2f8f449b5
Christian Wookey <christian.wookey@gmail.com>
2014-11-14 15:43:25 -0800

    changed the visuals slightly

--- visualizer.rkt
+++ visualizer.rkt
@@ -6,13 +6,12 @@
 (require 2htdp/universe)
 (require racket/base)
 
-;(define SONG-LOCATION "C:/tmp/rct2theme.wav")
+;(define SONG-LOCATION "songs/rct2theme.wav")
 (define SONG-LOCATION "songs/The Intro.wav")
 
 (define SONG (rs-read/clip SONG-LOCATION 0 (* 44100 120)))
 
 (define-struct world[t a c1now c1go slide-h slide-v drag? c1r c1g c1b])
-
 
 
 (define INITIAL-WORLD (make-world 0 0 300 300 900 650 false 100 100 100))
@@ -22,27 +21,45 @@
 (define (both a b) b)
 
 (define (tock w)
- (cond
-   [(= 0 (remainder (world-t w) 5))
-    (make-world
-     (add1 (world-t w))
-     (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
-     (/ (+ (world-c1now w) (world-c1go w)) 2)
-     (* (world-a w) 400)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
-    ]
-   [else
-    (make-world
-     (add1 (world-t w))
-     (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
-     (/ (+ (world-c1now w) (world-c1go w)) 2)
-     (world-c1go w)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
+  (cond
+    [(= 0 (remainder (world-t w) 2))
+     (make-world
+      (add1 (world-t w))
+      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
+      (/ (+ (world-c1now w) (world-c1go w)) 2)
+      (* (world-a w) 400)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
+     ]
+    [else
+     (make-world
+      (add1 (world-t w))
+      (abs (inexact->exact (rs-ith/left SONG (pstream-current-frame ps))))
+      (/ (+ (world-c1now w) (world-c1go w)) 2)
+      (world-c1go w)(world-slide-h w)(world-slide-v w)(world-drag? w)(world-c1r w)(world-c1g w)(world-c1b w))
     ]
  ))
 
 
+
 (define (draw w)
   (place-image
-  (circle (world-c1now w) "solid" (make-color (world-c1r w)(world-c1g w)(world-c1b w)))
+   (overlay
+   (beside
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "red")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "teal")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "blue")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "blue")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "teal")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    (ellipse (world-c1now w) (* 1.1 (world-c1now w)) "solid" "red")
+    (rectangle (/ 400 (world-c1now w)) 40 0 "white")
+    )
+    (rotate (* 20 (sin (world-t w))) (rectangle 1100 (/ (world-c1now w) 10) "solid" "red"))
+   )
   600 320
   (place-image
   (square 20 "solid" "green")
@@ -92,7 +109,7 @@
 
 (define (key-handler w key)
   (begin
-    (stop)
+    (if (key=? key " ") (stop) 0)
     w
     ))
 
